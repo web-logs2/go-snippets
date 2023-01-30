128bb4528b3d1b33be4f5cd1eee8a2ae9d738aed
darjun <darjun@126.com>
2023-01-30 22:40:12 +0800

    rate limit - per user

--- ratelimit-demo/limit.go
+++ ratelimit-demo/limit.go
@@ -1,14 +1,47 @@
 package main
 
 import (
-	"golang.org/x/time/rate"
+	"log"
+	"net"
 	"net/http"
+	"sync"
+
+	"golang.org/x/time/rate"
 )
 
-var limiter = rate.NewLimiter(1, 3)
+// Create a map to hold the rate limiter for each visitor and a mutex.
+var visitors = make(map[string]*rate.Limiter)
+var mu sync.Mutex
+
+// Retrieve and return the rate limiter for the current visitor if it
+// already exists. Otherwise, create a new rate limiter and add it to
+// the visitor map, using the IP address as the key.
+func getVisitor(ip string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	limiter, exists := visitors[ip]
+	if !exists {
+		limiter = rate.NewLimiter(1, 3)
+		visitors[ip] = limiter
+	}
+
+	return limiter
+}
 
 func limit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get the IP address for the current user.
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		// Call the getVisitor function to retrieve the rate limiter for
+		// the current user.
+		limiter := getVisitor(ip)
 		if limiter.Allow() == false {
 			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
 			return
