603a153750bbc54390a0fb6e43bb85218c3277f6
darjun <darjun@126.com>
2023-02-01 22:01:10 +0800

    validating flag values

---
+++ validating-flag-values/go.mod
@@ -0,0 +1,3 @@
+module github.com/darjun/validating-flag-values
+
+go 1.18
--- validating-flag-values/main.go
+++ validating-flag-values/main.go
@@ -1,1 +1,28 @@
-package validating_flag_values
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var (
+		environment string = "development"
+	)
+
+	flag.Func("environment", "Operating environment", func(flagValue string) error {
+		for _, allowedValue := range []string{"development", "staging", "production"} {
+			if flagValue == allowedValue {
+				environment = flagValue
+				return nil
+			}
+		}
+
+		return errors.New(`must be one of "development", "staging" or "production""`)
+	})
+
+	flag.Parse()
+
+	fmt.Printf("The operating environment is: %s\n", environment)
+}
