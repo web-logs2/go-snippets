dfb9c1117232a01ef9403cea789745d620b46581
darjun <darjun@126.com>
2023-02-18 01:02:16 +0800

    i18n - use gotext

--- i18n/bookstore/cmd/www/handlers.go
+++ i18n/bookstore/cmd/www/handlers.go
@@ -1,21 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	// Import the internal/translations package, so that its init()
+	// function is called.
+	_ "bookstore.example.com/internal/translations"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	// Extract the locale from the URL path. This line of code is likely to
-	// be different for you if you are using an alternative router.
 	locale := r.URL.Query().Get(":locale")
 
-	// If the locale matches one of our supported values, echo the locale
-	// in the response. Otherwise, send a 404 Not Found response.
+	// Declare variable to hold the target language tag.
+	var lang language.Tag
+
+	// Use language.MustParse() to assign the appropriate language tag
+	// for the locale.
 	switch locale {
-	case "en-gb", "de-de", "fr-ch":
-		fmt.Fprintf(w, "The locale is %s\n", locale)
+	case "en-gb":
+		lang = language.MustParse("en-GB")
+	case "de-de":
+		lang = language.MustParse("de-DE")
+	case "fr-ch":
+		lang = language.MustParse("fr-CH")
 	default:
 		http.NotFound(w, r)
+		return
 	}
+
+	// Initialize a message printer which uses the target language.
+	p := message.NewPrinter(lang)
+	// Print the welcome message translated into the target language.
+	p.Fprintf(w, "Welcome!\n")
 }
--- i18n/bookstore/go.mod
+++ i18n/bookstore/go.mod
@@ -3,3 +3,5 @@ module bookstore.example.com
 go 1.18
 
 require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+
+require golang.org/x/text v0.7.0 // indirect
